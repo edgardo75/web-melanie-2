8411e71fd90c248ae4fcd9cead16a9a84117d774
edgardo <edgardo75@gmail.com>
2015-11-20 14:28:51 -0300

    ultimoComit 20/11/2015

--- WebMelanie2/src/java/com/server/ShowReportNotaPedido.java
+++ WebMelanie2/src/java/com/server/ShowReportNotaPedido.java
@@ -1,10 +1,4 @@
 package com.server;
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
 import utilities.Reportes;
 import com.melani.ejb.ServiceNotaPedido;
 import java.io.IOException;
@@ -26,7 +20,6 @@ import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperRunManager;
 import net.sf.jasperreports.engine.data.JRXmlDataSource;
 import org.apache.log4j.Logger;
-
 import org.w3c.dom.Document;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
@@ -36,17 +29,8 @@ import org.xml.sax.SAXException;
  */
 public class ShowReportNotaPedido extends HttpServlet {
     @WebServiceRef(wsdlLocation = "WEB-INF/wsdl/localhost_8080/ServiceNotaPedido/NotaPedidoWs.wsdl")
-    private ServiceNotaPedido service;
-    
-    
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    private static final Logger logger = Logger.getLogger(ShowReportNotaPedido.class);
+    private ServiceNotaPedido service;    
+    private static final Logger LOGGER = Logger.getLogger(ShowReportNotaPedido.class);
     
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
@@ -54,24 +38,20 @@ public class ShowReportNotaPedido extends HttpServlet {
         response.setHeader("Pragma","no-cache");
         response.setDateHeader ("Expires", 0);
         response.setContentType("application/pdf");       
-        ServletOutputStream servletOutputStream =null;
-        String nronota="";
-        Integer idnota =0;
-        java.lang.String result="";
+        ServletOutputStream servletOutputStream;
+        String nronota;
+        Integer idnota;
+        java.lang.String result = null;
         try {
             nronota=request.getParameter("nronota");
             idnota = Integer.valueOf(nronota);
-            try { // Call Web Service Operation
+            try {
                 com.melani.ejb.NotaPedidoWs port = service.getNotaPedidoWsPort();
-                // TODO initialize WS operation arguments here
-                // TODO process result here
+                
                 result = port.selectUnaNota(idnota);
-            } catch (Exception ex) {
-           
-                // TODO handle custom exceptions here
-                logger.error("Error al llamar la operacion del webservice!!! "+ex.getMessage());
-            }
-            
+            } catch (Exception ex) {           
+                LOGGER.error("Error al llamar la operacion del webservice!!! "+ex.getMessage());
+            }            
             servletOutputStream=response.getOutputStream();
             DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
             DocumentBuilder db =null;
@@ -79,30 +59,24 @@ public class ShowReportNotaPedido extends HttpServlet {
                         db = dbf.newDocumentBuilder();
                     } catch (ParserConfigurationException ex) {
                
-                        logger.error("Error en Servlet ShowReportNotaPedido "+ex.getLocalizedMessage());
+                        LOGGER.error("Error en Servlet ShowReportNotaPedido "+ex.getLocalizedMessage());
                     }
                      InputSource is = new InputSource();
-                        is.setCharacterStream(new StringReader(result));
-                        //----------------------------------------------------------------------------
+                        is.setCharacterStream(new StringReader(result));                        
                             Document doc = null;
                                 try {
                                     doc = db.parse(is);
                                 } catch (SAXException ex) {
-           
-                                    logger.error("Error en Servlet ShowReportNotaPedido "+ex.getLocalizedMessage());
-                                }
-                            //----------------------------------------------------------------------------
-                            //----------------------------------------------------------------------------
+                                       LOGGER.error("Error en Servlet ShowReportNotaPedido "+ex.getLocalizedMessage());
+                                }                    
                              JRXmlDataSource xmlDataSource = null;
                             try {
                                 xmlDataSource = new JRXmlDataSource(doc, "/Lista");
                             } catch (JRException ex) {
                           
-                                logger.error("Error en Servlet ShowReportNotaPedido "+ex.getLocalizedMessage());
-                            }
-                            //----------------------------------------------------------------------------
-                                byte[] bytes = null;
-                            //----------------------------------------------------------------------------
+                                LOGGER.error("Error en Servlet ShowReportNotaPedido "+ex.getLocalizedMessage());
+                            }                            
+                                byte[] bytes;                            
                              try {
                                     Map map = new HashMap();
                                     map.put("SUBREPORT_DIR","");
@@ -110,44 +84,28 @@ public class ShowReportNotaPedido extends HttpServlet {
                                         response.setContentType("application/pdf");
                                         response.setContentLength(bytes.length);
                                         servletOutputStream.write(bytes, 0, bytes.length);
-                                        servletOutputStream.flush();
-                                      
-                                } catch (JRException e) {
-                                        // display stack trace in the browser
-                          
+                                        servletOutputStream.flush();                                      
+                                } catch (JRException e) {                                 
                                         StringWriter stringWriter = new StringWriter();
                                         PrintWriter printWriter = new PrintWriter(stringWriter);
                                         e.printStackTrace(printWriter);
                                         response.setContentType("text/plain");
                                         response.getOutputStream().print(stringWriter.toString());
-                                        logger.error(stringWriter.toString(), e);
+                                        LOGGER.error(stringWriter.toString(), e);
                                 }
            //-------------------------------------------------------------------------------
         }catch(  IOException | NumberFormatException ex){
           
-            logger.error("Error en el Servlet ShowreportNotaPedido "+ex.getMessage());
+            LOGGER.error("Error en el Servlet ShowreportNotaPedido "+ex.getMessage());
         }
     }
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
         processRequest(request, response);
     }
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
--- WebMelanie2/src/java/com/server/ShowReportPresupuesto.java
+++ WebMelanie2/src/java/com/server/ShowReportPresupuesto.java
@@ -1,10 +1,4 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.server;
-
-
 import utilities.Reportes;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -13,7 +7,6 @@ import java.io.StringWriter;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
-
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
@@ -27,84 +20,57 @@ import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperRunManager;
 import net.sf.jasperreports.engine.data.JRXmlDataSource;
 import org.apache.log4j.Logger;
-
-
 import org.w3c.dom.Document;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
-/**
- *
- * @author Edgardo
- */
+
 public class ShowReportPresupuesto extends HttpServlet {
     @WebServiceRef(wsdlLocation = "WEB-INF/wsdl/localhost_8080/ServicesPresupuestos/PresupuestoWs.wsdl")
     private com.melani.ejb.ServicesPresupuestos service;
-    
-    
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    private static final Logger logger = Logger.getLogger(ShowReportPresupuesto.class);
+
+    private static final Logger LOGGER = Logger.getLogger(ShowReportPresupuesto.class);
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException, SQLException {
        response.setHeader("Cache-Control","no-cache");  //Para evitar el cache
         response.setHeader("Pragma","no-cache");
         response.setDateHeader ("Expires", 0);
         response.setContentType("application/pdf");        
-        ServletOutputStream servletOutputStream =null;
+        ServletOutputStream servletOutputStream;
         String xml = "";
-        String presnro="";
-        Integer idpresupuesto=0;
-        try {
-            
-                presnro=request.getParameter("presnro");
-                
+        String presnro;
+        Integer idpresupuesto;
+        try {            
+                presnro=request.getParameter("presnro");                
                 idpresupuesto=Integer.valueOf(presnro);
-                try { // Call Web Service Operation
-                    com.melani.ejb.PresupuestoWs port = service.getPresupuestoWsPort();
-                    // TODO initialize WS operation arguments here
-                    // TODO process result here
+                try {
+                    com.melani.ejb.PresupuestoWs port = service.getPresupuestoWsPort();                    
                     xml = port.showReportPresupuesto(idpresupuesto);                    
-                } catch (Exception ex) {
-                    // TODO handle custom exceptions here
-                    logger.error("Error en Servlet ShowReportPresupuesto "+ex.getLocalizedMessage());
-                }                   
-               
-               
-               //----------------------------------------------------------------------------                
-                
+                } catch (Exception ex) {                 
+                    LOGGER.error("Error en Servlet ShowReportPresupuesto "+ex.getLocalizedMessage());
+                }
                     servletOutputStream = response.getOutputStream();              
-                    DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-                    
+                    DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();                    
                     DocumentBuilder db = null;
                             try {              
                                 db = dbf.newDocumentBuilder();
                             } catch (ParserConfigurationException ex) {
-                                logger.error("Error en Servlet ShowReportPresupuesto "+ex.getLocalizedMessage());
+                                LOGGER.error("Error en Servlet ShowReportPresupuesto "+ex.getLocalizedMessage());
                             }                    
                         InputSource is = new InputSource();                       
-                        is.setCharacterStream(new StringReader(xml));                       
-                        //----------------------------------------------------------------------------
+                        is.setCharacterStream(new StringReader(xml));                                               
                             Document doc = null;
                                 try {                       
                                     doc = db.parse(is);
                                 } catch (SAXException ex) {
-                                        logger.error("Error en Servlet ShowReportPresupuesto "+ex.getLocalizedMessage());
-                                }
-                            //----------------------------------------------------------------------------                            
+                                        LOGGER.error("Error en Servlet ShowReportPresupuesto "+ex.getLocalizedMessage());
+                                }                            
                              JRXmlDataSource xmlDataSource = null;
                             try {                                
                                 xmlDataSource = new JRXmlDataSource(doc, "/Lista/Item");
                             } catch (JRException ex) {
-                                logger.error("Error en Servlet ShowReportPresupuesto "+ex.getLocalizedMessage());
-                            }
-                            //----------------------------------------------------------------------------
-                                byte[] bytes = null;
-                            //----------------------------------------------------------------------------
+                                LOGGER.error("Error en Servlet ShowReportPresupuesto "+ex.getLocalizedMessage());
+                            }                     
+                                byte[] bytes;                     
                     try {          
                                     Map map = new HashMap();
                                     map.put("SUBREPORT_DIR","");
@@ -121,49 +87,33 @@ public class ShowReportPresupuesto extends HttpServlet {
                             e.printStackTrace(printWriter);
                             response.setContentType("text/plain");
                             response.getOutputStream().print(stringWriter.toString());
-                            logger.error(stringWriter.toString(), e);
-                    }
-           //-------------------------------------------------------------------------------
+                            LOGGER.error(stringWriter.toString(), e);
+                    }           
         }catch(NullPointerException npe){
-            logger.error("Error en el Servlet ShowReportPresupuesto "+npe.getMessage());
+            LOGGER.error("Error en el Servlet ShowReportPresupuesto "+npe.getMessage());
         } 
     }
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
         try {
             processRequest(request, response);
         }catch(NullPointerException npe)    {
-            logger.error("Error en Servlet ShowReportPresupuestos ",npe.getCause());
+            LOGGER.error("Error en Servlet ShowReportPresupuestos ",npe.getCause());
         } catch (SQLException ex) {
-            logger.error("Error en Servlet ShowReportPresupuesto "+ex.getMessage());
+            LOGGER.error("Error en Servlet ShowReportPresupuesto "+ex.getMessage());
         }
     }
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
         try {
             processRequest(request, response);
         } catch (SQLException ex) {
-            logger.error("Error en Servlet ShowReportPresupuesto");
+            LOGGER.error("Error en Servlet ShowReportPresupuesto");
             
         }
-    }
-
-    
+    }    
 }
